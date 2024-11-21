49e83c5624c72d34c91f2dea13cfec3452d9d23d
H0meqube <>
2024-11-21 13:54:34 +0900

    shibayamaに変換

--- app/src/main/java/com/example/commit_test_24_11_21/MainActivity.kt
+++ app/src/main/java/com/example/commit_test_24_11_21/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     COMMIT_TEST_24_11_21Theme {
-        Greeting("Android")
+        Greeting("Shibayama")
     }
 }
